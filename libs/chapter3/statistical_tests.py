d7e8526180e58ea204541610dcd22d63af19f857
Miguel Matey Sanz <al341802@uji.es>
2024-05-27 09:29:30 +0200

    chap3: include info to statistical tests

--- libs/chapter3/statistical_tests.py
+++ libs/chapter3/statistical_tests.py
@@ -20,6 +20,7 @@ import numpy as np
 import pandas as pd
 import pingouin as pg
 
+from libs.chapter3.model import ActivityMetric, Filter, ModelMetric, TargetFilter 
 
 
 def pairwise_n_comparision(data, filters, alternative='two-sided', stars=False, parametric=False):
@@ -78,3 +79,50 @@ def p_value_formatter(stars, decimals=3):
             return f'{val:.{decimals}}'
         
     return as_stars if stars else as_value
+
+
+def is_parametric_data(reports, models, sources):
+    '''
+    Determines if the results in the reports follow a parametric or a non-parametric distribution.
+
+    Args:
+        reports (`pandas.DataFrame`): Model reports.
+        models (`libs.chapter3.model.Models`[]): List with the models.
+        sources (`libs.chapter3.model.Source`[]): List with the data sources.
+        
+    Returns:
+        (`pandas.DataFrame`): DataFrame indicating if the results from a model+source are parametric (`True`) or not (`False`).
+    '''
+
+    dfs = []
+    for model in models:
+        for source in sources:
+            df = _check_normality(Filter(model, source, None, None).apply(reports)).all().to_frame().transpose()
+            df['model'] = model
+            df['source'] = source
+            dfs.append(df)
+
+    return pd.concat(dfs).set_index(['model', 'source'])
+
+
+def _check_normality(data):
+    to_check = [(TargetFilter.MODEL, ModelMetric.ACCURACY), 
+            (TargetFilter.SEATED, ActivityMetric.F1),
+            (TargetFilter.STANDING_UP, ActivityMetric.F1),
+            (TargetFilter.WALKING, ActivityMetric.F1),
+            (TargetFilter.TURNING, ActivityMetric.F1),
+            (TargetFilter.SITTING_DOWN, ActivityMetric.F1)]
+
+    normality_table = np.full((22, 6), False)
+    columns = []
+
+    for i, (target_filter, metric) in enumerate(to_check):
+        filtered = Filter(None, None, target_filter, metric).apply(data)
+        columns.append(f'{target_filter} {metric}')
+        for n in range(1, 23):
+            filtered_n = filtered[filtered.n == n]
+            normal = pg.normality(filtered_n['value']).loc['value']['normal']
+            if normal:
+                normality_table[n-1, i] = True
+
+    return pd.DataFrame(normality_table, index=np.arange(1,23), columns=columns)
